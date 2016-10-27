8b6129002a802e58da1435d2f1aa9a9accff6722
kadirmuratbaseren <kadirmuratbaseren@gmail.com>
2016-10-27 14:21:42 +0300

    Edited assembly info

--- Aspnet-mvc-template-cms-sample/Properties/AssemblyInfo.cs
+++ Aspnet-mvc-template-cms-sample/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Aspnet_mvc_template_cms_sample")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("KMB PageField JS")]
+[assembly: AssemblyDescription("Includes PageFieldJS file and on edit mode you can change text fields on runtime. Making changes save to sql database with EF CodeFirst and JQuery ajax.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Kadir Murat Başeren")]
 [assembly: AssemblyProduct("Aspnet_mvc_template_cms_sample")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyCopyright("Copyright © Kadir Murat Başeren  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
